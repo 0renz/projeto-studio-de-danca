dd62c088b4126bb043d520016452ada65493d44c
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2024-04-07 11:04:54 -0300

    teste: primeiro commit!

---
+++ projeto-estudio-de-danca/src/main/java/teste/projeto/estudio/de/danca/ProjetoEstudioDeDanca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package teste.projeto.estudio.de.danca;
+
+/**
+ *
+ * @author loren
+ */
+public class ProjetoEstudioDeDanca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
