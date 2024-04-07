fc835dbf0e0cae4304d71a5bae20439e1201924c
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2024-04-07 11:45:56 -0300

    refactor: mudança de nome (de novo)

---
+++ projeto-studio-de-danca/src/main/java/projeto_studio_de_danca/ProjetoStudioDeDanca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package projeto_studio_de_danca;
+
+/**
+ *
+ * @author loren
+ */
+public class ProjetoStudioDeDanca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
